e93c25e43e7eafa4b697e650f425b02639d9581e
CWelsh37 <33524347+CWelsh37@users.noreply.github.com>
2017-11-09 12:07:38 -0500

    Initial

---
+++ personality_CW.py
@@ -0,0 +1,16 @@
+name = "Connor Welsh"
+food = "Chick-Fil-A, Cheesburgers, Pasta, Mac and Cheese"
+country = "The United States of America"
+sport = "Hockey"
+
+
+print (name + " likes to eat " + food)
+print (name + " likes to play " + sport)
+print (name + " favorite country is " + country)
+
+print ("What's your favorite subject")
+subject = input()
+
+if subject == "Math":
+    print ("Thats my favorite too!!")
+    
